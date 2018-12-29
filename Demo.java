f32c717c29669367213a3c0deb2061b2a881d291
pradeepkumar2060 <vagrant@git.example.com>
2018-12-29 13:41:20 +0000

    second time content added to demo.java file

--- Demo.java
+++ Demo.java
@@ -2,6 +2,7 @@ class Demo
 {
   public static void main(String args [])
  {
-    System.out.println("first commit");
+    System.out.println("First Commit");
+    System.out.println("Second Commit");
  }	
 }
